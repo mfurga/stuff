3a98a32ed7fa5a4f56bbbf43e22b99d1c532caf0
Mateusz Furga <matfurga@gmail.com>
2023-10-22 14:22:12 +0200

    Update packer

--- elf_packer/elf64.py
+++ elf_packer/elf64.py
@@ -4,359 +4,404 @@ import sys
 import struct
 from pprint import pprint
 
-ELF_MAGIC = b"\x7fELF"
-
-EI_CLASS = 4
-EI_DATA = 5
-EI_VERSION = 6
-EI_OSABI = 7
-EI_ABIVERSION = 8
-EI_NIDENT = 16
-
-ELFCLASS32 = 1
-ELFCLASS64 = 2
-
-ELFDATA2LSB = 1
-ELFDATA2MSB = 2
-
-ELFOSABI_SYSV = 0
-ELFOSABI_HPUX = 1
-ELFOSABI_STANDALONE = 255
-
-# Object file types
-ET_NONE = 0
-ET_REL = 1
-ET_EXEC = 2
-ET_DYN = 3
-ET_CORE = 4
-
-def et_str(v):
-  ET_STR = {
-    ET_NONE: "None",
-    ET_REL: "Relocatable object",
-    ET_EXEC: "Executable",
-    ET_DYN: "Shared object",
-    ET_CORE: "Core"
-  }
-  if v in ET_STR:
-    return ET_STR[v]
-  return "UNKNOWN"
-
-# Program header types
-PT_NULL = 0
-PT_LOAD = 1
-PT_DYNAMIC = 2
-PT_INTERP = 3
-PT_NOTE = 4
-PT_SHLIB = 5
-PT_PHDR = 6
-PT_LOOS = 0x60000000
-PT_HIOS = 0x6FFFFFFF
-PT_LOPROC = 0x70000000
-PT_HIPROC = 0x7FFFFFFF
-
-def pt_str(v):
-  PT_STR = {
-    PT_NULL: "NULL",
-    PT_LOAD: "LOAD",
-    PT_DYNAMIC: "DYNAMIC",
-    PT_INTERP: "INTERP",
-    PT_NOTE: "NOTE",
-    PT_SHLIB: "SHLIB",
-    PT_PHDR: "PHDR"
-  }
-  if v in PT_STR:
-    return PT_STR[v]
-  if PT_LOOS <= v <= PT_HIOS:
-    return "OS"
-  if PT_LOPROC <= v <= PT_HIPROC:
-    return "PROC"
-  return "UNKNOWN"
-
-# Program header flags
-PF_X = 1
-PF_W = 2
-PF_R = 3
-
-def pf_str(v):
-  return "{}{}{}".format(
-    "R" if v & PF_R else " ",
-    "W" if v & PF_W else " ",
-    "X" if v & PF_X else " "
-  )
-
-# Section types
-SHT_NULL = 0
-SHT_PROGBITS = 1
-SHT_SYMTAB = 2
-SHT_STRTAB = 3
-SHT_RELA = 4
-SHT_HASH = 5
-SHT_DYNAMIC = 6
-SHT_NOTE = 7
-SHT_NOBITS = 8
-SHT_REL = 9
-SHT_SHLIB = 10
-SHT_DYNSYM = 11
-SHT_INIT_ARRAY = 14
-SHT_FINI_ARRAY = 15
-SHT_LOOS = 0x60000000
-SHT_HIOS = 0x6FFFFFFF
-SHT_LOPROC = 0x70000000
-SHT_HIPROC = 0x7FFFFFFF
-
-def sht_str(v):
-  SHT_STR = {
-    SHT_NULL: "NULL",
-    SHT_PROGBITS: "PROGBITS",
-    SHT_SYMTAB: "SYMTAB",
-    SHT_STRTAB: "STRTAB",
-    SHT_RELA: "RELA",
-    SHT_HASH: "HASH",
-    SHT_DYNAMIC: "DYNAMIC",
-    SHT_NOTE: "NOTE",
-    SHT_NOBITS: "NOBITS",
-    SHT_REL: "REL",
-    SHT_SHLIB: "SHLIB",
-    SHT_DYNSYM: "DYNSYM",
-    SHT_INIT_ARRAY: "INIT ARRAY",
-    SHT_FINI_ARRAY: "FINI ARRAY"
-  }
-  if v in SHT_STR:
-    return SHT_STR[v]
-  if SHT_LOOS <= v <= SHT_HIOS:
-    return "OS"
-  if SHT_LOPROC <= v <= SHT_HIPROC:
-    return "PROC"
-  return "UNKNOWN"
-
-# Section flags
-SHF_WRITE = 0x1
-SHF_ALLOC = 0x2
-SHF_EXECINSTR = 0x4
-
-def shf_str(v):
-  return "{}{}{}".format(
-    "A" if v & SHF_ALLOC else " ",
-    "W" if v & SHF_WRITE else " ",
-    "E" if v & SHF_EXECINSTR else " "
-  )
-
-def elf_half(data, off):
-  return struct.unpack("<H", data[off:off + 2])[0]
-
-def elf_word(data, off):
-  return struct.unpack("<I", data[off:off + 4])[0]
-
-def elf_xword(data, off):
-  return struct.unpack("<Q", data[off:off + 8])[0]
-
-def elf_addr(data, off):
-  return elf_xword(data, off)
-
-def elf_off(data, off):
-  return elf_xword(data, off)
-
 def elf_section_name(str):
   return str[:str.find(b"\x00")].decode("utf-8")
 
-class ELF64:
-  def __init__(self, data: bytearray) -> None:
-    self._data = data
-    self._parse_elf()
+class Header:
+  # ELF types
+  ET_NONE = 0
+  ET_REL = 1
+  ET_EXEC = 2
+  ET_DYN = 3
+  ET_CORE = 4
 
-  def _parse_elf(self):
-    self._hdr = self._parse_elf_header(self._data[:64])
+  def __init__(self):
+    self.ident = b""
+    self.type = None
+    self.machine = None
+    self.version = None
+    self.entry = None
+    self.phoff = None
+    self.shoff = None
+    self.flags = None
+    self.ehsize = None
+    self.phentsize = None
+    self.phnum = None
+    self.shentsize = None
+    self.shnum = None
+    self.shstrndx = None
 
-    # Parse program header
-    e_phoff = self._hdr["e_phoff"]
-    e_phentsize = self._hdr["e_phentsize"]
-    e_phnum = self._hdr["e_phnum"]
+  @classmethod
+  def load_from_bytes(cls, data: bytes):
+    header = cls()
+    header.unpack(data)
+    return header
 
-    self._phdrs = []
-    for i in range(e_phnum):
-      begin = e_phoff + i * e_phentsize
-      end = e_phoff + (i + 1) * e_phentsize
+  def unpack(self, data: bytes) -> None:
+    (
+      *self.ident,
+      self.type,
+      self.machine,
+      self.version,
+      self.entry,
+      self.phoff,
+      self.shoff,
+      self.flags,
+      self.ehsize,
+      self.phentsize,
+      self.phnum,
+      self.shentsize,
+      self.shnum,
+      self.shstrndx
+    ) = struct.unpack("<16BHHI3QI6H", data)
 
-      assert(begin < len(self._data))
-      assert(end <= len(self._data))
+  def pack(self) -> bytes:
+    return struct.pack("<16BHHI3QI6H",
+      *self.ident,
+      self.type,
+      self.machine,
+      self.version,
+      self.entry,
+      self.phoff,
+      self.shoff,
+      self.flags,
+      self.ehsize,
+      self.phentsize,
+      self.phnum,
+      self.shentsize,
+      self.shnum,
+      self.shstrndx)
 
-      self._phdrs.append(
-        self._parse_elf_program_header(self._data[begin:end]))
-
-    # Parse section header
-    e_shoff = self._hdr["e_shoff"]
-    e_shnum = self._hdr["e_shnum"]
-    e_shentsize = self._hdr["e_shentsize"]
-
-    self._shdrs = []
-    for i in range(e_shnum):
-      begin = e_shoff + i * e_shentsize
-      end = e_shoff + (i + 1) * e_shentsize
-
-      assert(begin < len(self._data))
-      assert(end <= len(self._data))
-
-      self._shdrs.append(
-        self._parse_elf_section_header(self._data[begin:end]))
-
-    self._data = bytearray(self._data[self._phdrs_end_offset:e_shoff])
-
-  @property
-  def _phdrs_end_offset(self):
-    return self._hdr["e_phoff"] + self._hdr["e_phentsize"] * self._hdr["e_phnum"]
-
-  def _parse_elf_header(self, hd):
-    hdr = {}
-
-    e_ident = hd[0:15 + 1]
-
-    # Parser supports only little-endian ELF64 binaries
-    assert(e_ident[EI_CLASS] == ELFCLASS64)
-    assert(e_ident[EI_DATA] == ELFDATA2LSB)
-
-    hdr["e_ident"] = e_ident
-    hdr["e_type"] = elf_half(hd, 16)
-    hdr["e_machine"] = elf_half(hd, 18)
-    hdr["e_version"] = elf_word(hd, 20)
-    hdr["e_entry"] = elf_addr(hd, 24)
-    hdr["e_phoff"] = elf_off(hd, 32)
-    hdr["e_shoff"] = elf_off(hd, 40)
-    hdr["e_flags"] = elf_word(hd, 48)
-    hdr["e_ehsize"] = elf_half(hd, 52)
-    hdr["e_phentsize"] = elf_half(hd, 54)
-    hdr["e_phnum"] = elf_half(hd, 56)
-    hdr["e_shentsize"] = elf_half(hd, 58)
-    hdr["e_shnum"] = elf_half(hd, 60)
-    hdr["e_shstrndx"] = elf_half(hd, 62)
-
-    return hdr
-
-  def _parse_elf_program_header(self, ph):
-    phdr = {}
-
-    phdr["p_type"] = elf_word(ph, 0)
-    phdr["p_flags"] = elf_word(ph, 4)    
-    phdr["p_offset"] = elf_off(ph, 8)
-    phdr["p_vaddr"] = elf_off(ph, 16)
-    phdr["p_paddr"] = elf_off(ph, 24)
-    phdr["p_filesz"] = elf_xword(ph, 32)
-    phdr["p_memsz"] = elf_xword(ph, 40)
-    phdr["p_align"] = elf_xword(ph, 48)
-
-    if phdr["p_offset"] >= self._phdrs_end_offset:
-      # offset relative to beginning of sections / segments
-      phdr["p_offset"] -= self._phdrs_end_offset
-
-    return phdr
-
-  def _parse_elf_section_header(self, sh):
-    shdr = {}
-
-    shdr["sh_name"] = elf_word(sh, 0)
-    shdr["sh_type"] = elf_word(sh, 4)
-    shdr["sh_flags"] = elf_xword(sh, 8)
-    shdr["sh_addr"] = elf_addr(sh, 16)
-    shdr["sh_offset"] = elf_off(sh, 24)
-    shdr["sh_size"] = elf_xword(sh, 32)
-    shdr["sh_link"] = elf_word(sh, 40)
-    shdr["sh_info"] = elf_word(sh, 44)
-    shdr["sh_addralign"] = elf_xword(sh, 48)
-    shdr["sh_entsize"] = elf_xword(sh, 56)
-
-    if shdr["sh_offset"] >= self._phdrs_end_offset:
-      # offset relative to beginning of sections / segments
-      shdr["sh_offset"] -= self._phdrs_end_offset
-
-    return shdr
-
-  def add_section(self, data: bytearray, name: str, type: int, flags: int,
-                  address: int) -> None:
-    shdr = {
-      "sh_name": 0,
-      "sh_type": type,
-      "sh_flags": flags,
-      "sh_addr": address,
-      "sh_offset": len(self._data),
-      "sh_size": len(data),
-      "sh_link": 0,
-      "sh_info": 0,
-      "sh_addralign": 0,
-      "sh_entsize": 0
+  @staticmethod
+  def type_str(type: int) -> str:
+    ET_STR = {
+      Header.ET_NONE: "None",
+      Header.ET_REL: "Relocatable object",
+      Header.ET_EXEC: "Executable",
+      Header.ET_DYN: "Shared object",
+      Header.ET_CORE: "Core"
     }
+    if type in ET_STR:
+      return ET_STR[type]
+    return "UNKNOWN"
 
-    self._shdrs.append(shdr)
-    self._data.extend(data)
+  def __str__(self) -> str:
+    s = []
+    s.append("{:<34}: {} ({})".format("Type", self.type_str(self.type), self.type))
+    s.append("{:<34}: {}".format("Machine", self.machine))
+    s.append("{:<34}: {}".format("Version", self.version))
+    s.append("{:<34}: 0x{:x}".format("Entry point", self.entry))
+    s.append("{:<34}: {}".format("Program header offset", self.shoff))
+    s.append("{:<34}: {}".format("Section header offset", self.phoff))
+    s.append("{:<34}: {}".format("Flags", self.flags))
+    s.append("{:<34}: {}".format("Header size", self.ehsize))
+    s.append("{:<34}: {}".format("Size of program header entry", self.phentsize))
+    s.append("{:<34}: {}".format("Number of program header entries", self.phnum))
+    s.append("{:<34}: {}".format("Size of section header entry", self.shentsize))
+    s.append("{:<34}: {}".format("Number of section header entries", self.shnum))
+    s.append("{:<34}: {}".format("Section name string table index", self.shstrndx))
+    return "\n".join(s)
 
-  def print(self):
-    data = self._data
-    hdr = self._hdr
-    shdrs = self._shdrs
-    phdrs = self._phdrs
+class Segment:
+  # Program header types
+  PT_NULL = 0
+  PT_LOAD = 1
+  PT_DYNAMIC = 2
+  PT_INTERP = 3
+  PT_NOTE = 4
+  PT_SHLIB = 5
+  PT_PHDR = 6
+  PT_LOOS = 0x60000000
+  PT_HIOS = 0x6FFFFFFF
+  PT_LOPROC = 0x70000000
+  PT_HIPROC = 0x7FFFFFFF
 
-    e_shstrndx = hdr["e_shstrndx"]
+  # Program header flags
+  PF_X = 0x1
+  PF_W = 0x2
+  PF_R = 0x4
 
-    assert(e_shstrndx < len(shdrs))
+  def __init__(self) -> None:
+    self.type = None
+    self.flags = None
+    self.offset = None,
+    self.vaddr = None
+    self.paddr = None
+    self.filesz = None
+    self.memsz = None
+    self.align = None
 
-    # print hdr info
-    print("ELF info:")
-    print("{:<34}: {} ({})".format(
-      "Type", et_str(hdr["e_type"]), hdr["e_type"]))
-    print("{:<34}: {}".format("Machine", hdr["e_machine"]))
-    print("{:<34}: {}".format("Version", hdr["e_version"]))
-    print("{:<34}: 0x{:x}".format("Entry point", hdr["e_entry"]))
-    print("{:<34}: {}".format("Program header offset", hdr["e_phoff"]))
-    print("{:<34}: {}".format("Section header offset", hdr["e_shoff"]))
-    print("{:<34}: {}".format("Flags", hdr["e_flags"]))
-    print("{:<34}: {}".format("Header size", hdr["e_ehsize"]))
-    print("{:<34}: {}".format("Size of program header entry",
-      hdr["e_phentsize"]))
-    print("{:<34}: {}".format("Number of program header entries",
-      hdr["e_phnum"]))
-    print("{:<34}: {}".format("Size of section header entry",
-      hdr["e_shentsize"]))
-    print("{:<34}: {}".format("Number of section header entries",
-      hdr["e_shnum"]))
-    print("{:<34}: {}".format("Section name string table index",
-      hdr["e_shstrndx"]))
+    self.sections = []
 
-    print()
-    print("Program header:")
-    print("{:<12}{:<8}{:<8}{:<12}{:<12}{:<8}{:<8}{:<8}".format(
+  @classmethod
+  def load_from_bytes(cls, data: bytes):
+    segment = cls()
+    segment.unpack(data)
+    return segment
+
+  def unpack(self, data: bytes) -> None:
+    (
+      self.type,
+      self.flags,
+      self.offset,
+      self.vaddr,
+      self.paddr,
+      self.filesz,
+      self.memsz,
+      self.align,
+    ) = struct.unpack("<II6Q", data)
+
+  def pack(self) -> bytes:
+    return struct.pack("<II6Q",
+      self.type,
+      self.flags,
+      self.offset,
+      self.vaddr,
+      self.paddr,
+      self.filesz,
+      self.memsz,
+      self.align)
+
+  @staticmethod
+  def type_str(type: int) -> str:
+    PT_STR = {
+      Segment.PT_NULL: "NULL",
+      Segment.PT_LOAD: "LOAD",
+      Segment.PT_DYNAMIC: "DYNAMIC",
+      Segment.PT_INTERP: "INTERP",
+      Segment.PT_NOTE: "NOTE",
+      Segment.PT_SHLIB: "SHLIB",
+      Segment.PT_PHDR: "PHDR"
+    }
+    if type in PT_STR:
+      return PT_STR[type]
+    if Segment.PT_LOOS <= type <= Segment.PT_HIOS:
+      return "OS"
+    if Segment.PT_LOPROC <= type <= Segment.PT_HIPROC:
+      return "PROC"
+    return "UNKNOWN"
+
+  @staticmethod
+  def flags_str(flags: int) -> str:
+    return "{}{}{}".format(
+      "R" if flags & Segment.PF_R else " ",
+      "W" if flags & Segment.PF_W else " ",
+      "X" if flags & Segment.PF_X else " "
+    )
+
+  def __str__(self) -> str:
+    return "{:<12}{:<8}{:<8}0x{:<10x}0x{:<10x}{:<8}{:<8}0x{:<6x}".format(
+      Segment.type_str(self.type),
+      Segment.flags_str(self.flags),
+      self.offset,
+      self.vaddr,
+      self.paddr,
+      self.filesz,
+      self.memsz,
+      self.align
+    )
+
+class Section:
+  # Section types
+  SHT_NULL = 0
+  SHT_PROGBITS = 1
+  SHT_SYMTAB = 2
+  SHT_STRTAB = 3
+  SHT_RELA = 4
+  SHT_HASH = 5
+  SHT_DYNAMIC = 6
+  SHT_NOTE = 7
+  SHT_NOBITS = 8
+  SHT_REL = 9
+  SHT_SHLIB = 10
+  SHT_DYNSYM = 11
+  SHT_INIT_ARRAY = 14
+  SHT_FINI_ARRAY = 15
+  SHT_LOOS = 0x60000000
+  SHT_HIOS = 0x6FFFFFFF
+  SHT_LOPROC = 0x70000000
+  SHT_HIPROC = 0x7FFFFFFF
+
+  # Section flags
+  SHF_WRITE = 0x1
+  SHF_ALLOC = 0x2
+  SHF_EXECINSTR = 0x4
+
+  def __init__(self) -> None:
+    self.name = 0
+    self.type = 0
+    self.flags = 0
+    self.addr = 0
+    self.offset = 0
+    self.size = 0
+    self.link = 0
+    self.info = 0
+    self.addralign = 1
+    self.entsize = 0
+
+    self.content = bytes()
+
+  @classmethod
+  def load_from_bytes(cls, sh: bytes, data: bytes):
+    section = cls()
+    section.unpack(sh)
+
+    if section.type != Section.SHT_NOBITS:
+      start = section.offset
+      end = start + section.size
+      section.content = data[start:end]
+    return section
+
+  def unpack(self, data: bytes) -> None:
+    (
+      self.name,
+      self.type,
+      self.flags,
+      self.addr,
+      self.offset,
+      self.size,
+      self.link,
+      self.info,
+      self.addralign,
+      self.entsize
+    ) = struct.unpack("<II4QIIQQ", data)
+
+  def pack(self) -> bytes:
+    return struct.pack("<II4QIIQQ",
+      self.name,
+      self.type,
+      self.flags,
+      self.addr,
+      self.offset,
+      self.size,
+      self.link,
+      self.info,
+      self.addralign,
+      self.entsize)
+
+  @staticmethod
+  def type_str(type: int) -> str:
+    SHT_STR = {
+      Section.SHT_NULL: "NULL",
+      Section.SHT_PROGBITS: "PROGBITS",
+      Section.SHT_SYMTAB: "SYMTAB",
+      Section.SHT_STRTAB: "STRTAB",
+      Section.SHT_RELA: "RELA",
+      Section.SHT_HASH: "HASH",
+      Section.SHT_DYNAMIC: "DYNAMIC",
+      Section.SHT_NOTE: "NOTE",
+      Section.SHT_NOBITS: "NOBITS",
+      Section.SHT_REL: "REL",
+      Section.SHT_SHLIB: "SHLIB",
+      Section.SHT_DYNSYM: "DYNSYM",
+      Section.SHT_INIT_ARRAY: "INIT ARRAY",
+      Section.SHT_FINI_ARRAY: "FINI ARRAY"
+    }
+    if type in SHT_STR:
+      return SHT_STR[type]
+    if Section.SHT_LOOS <= type <= Section.SHT_HIOS:
+      return "OS"
+    if Section.SHT_LOPROC <= type <= Section.SHT_HIPROC:
+      return "PROC"
+    return "UNKNOWN"
+
+  @staticmethod
+  def flags_str(flags: int) -> str:
+    return "{}{}{}".format(
+      "A" if flags & Section.SHF_ALLOC else " ",
+      "W" if flags & Section.SHF_WRITE else " ",
+      "E" if flags & Section.SHF_EXECINSTR else " "
+    )
+
+  def __str__(self) -> str:
+    return "{:<20}{:<12}{:<6}0x{:<8x}{:<8}{:<8}".format(
+      self.name,
+      self.type_str(self.type),
+      self.flags_str(self.flags),
+      self.addr,
+      self.offset,
+      self.size,
+    )
+
+class ELF:
+  def __init__(self, data: bytearray) -> None:
+    self.header: Header = None
+    self.segments: list[Segment] = []
+    self.sections: list[Section] = []
+
+    self._parse_elf(data)
+
+  def _parse_elf(self, data: bytearray) -> None:
+    self.header = Header.load_from_bytes(data[:64])
+
+    for i in range(self.header.phnum):
+      start = self.header.phoff + i * self.header.phentsize
+      end = start + self.header.phentsize
+      self.segments.append(Segment.load_from_bytes(data[start:end]))
+
+    for i in range(self.header.shnum):
+      start = self.header.shoff + i * self.header.shentsize
+      end = start + self.header.shentsize
+      self.sections.append(Section.load_from_bytes(data[start:end], data))
+
+    # Map sections to segments
+    for segment in self.segments:
+      seg_start = segment.offset
+      seg_end = seg_start + segment.filesz - 1
+      for i, section in enumerate(self.sections):
+        sec_start = section.offset
+        sec_end = sec_start + section.size - 1
+
+        if seg_start <= sec_start <= seg_end and \
+           seg_start <= sec_end <= seg_end:
+           segment.sections.append(i)
+
+  def set_entry_point(self, entry: int) -> None:
+    self.header.entry = entry
+
+  def append_section(self, section: Section) -> None:
+    section.offset = self.sections[-1].offset + self.sections[-1].size
+
+    padding = 0
+    if section.offset % section.addralign != 0:
+      padding = section.addralign - (section.offset % section.addralign)
+      section.offset += padding
+
+    self.sections.append(section)
+    self.header.shnum += 1
+    self.header.shoff += section.size + padding
+
+  def data(self) -> bytes:
+    b = b""
+    b += self.header.pack()
+
+    b = b.ljust(self.header.phoff, b"\x00")
+    for i in range(self.header.phnum):
+      b += self.segments[i].pack()
+
+    for i in range(self.header.shnum):
+      b = b.ljust(self.sections[i].offset, b"\x00")
+      b += self.sections[i].content
+
+    b = b.ljust(self.header.shoff, b"\x00")
+    for i in range(self.header.shnum):
+      section = self.sections[i]
+      b += section.pack()
+
+    return b
+
+  def __str__(self):
+    s = []
+    s.append(str(self.header))
+    s.append("\nSegments:")
+    s.append("{:<12}{:<8}{:<8}{:<12}{:<12}{:<8}{:<8}{:<8}".format(
       "Type", "Flags", "Offset", "Vaddr", "Paddr", "Filesz", "Memsz", "Align"))
-
-    for phdr in phdrs:
-      print("{:<12}{:<8}{:<8}0x{:<10x}0x{:<10x}{:<8}{:<8}0x{:<6x}".format(
-        pt_str(phdr["p_type"]),
-        pf_str(phdr["p_flags"]),
-        phdr["p_offset"],
-        phdr["p_vaddr"],
-        phdr["p_paddr"],
-        phdr["p_filesz"],
-        phdr["p_memsz"],
-        phdr["p_align"]
-      ))
-
-    print()
-    print("Sections:")
-    print("{:<20}{:<12}{:<6}{:<10}{:<8}{:<8}".format(
+    for segment in self.segments:
+      s.append(str(segment))
+    s.append("\nSections:")
+    s.append("{:<20}{:<12}{:<6}{:<10}{:<8}{:<8}".format(
       "Name", "Type", "Flags", "Address", "Offset", "Size"))
+    for section in self.sections:
+      s.append(str(section))
+    return "\n".join(s)
 
-    shdr_str = shdrs[e_shstrndx]
-    s_str_offset = shdr_str["sh_offset"]
-    s_str_size = shdr_str["sh_size"]
-
-    for shdr in shdrs:
-      str_offset = s_str_offset + shdr["sh_name"]
-
-      print("{:<20}{:<12}{:<6}0x{:<8x}{:<8}{:<8}".format(
-        elf_section_name(data[str_offset:]),
-        sht_str(shdr["sh_type"]),
-        shf_str(shdr["sh_flags"]),
-        shdr["sh_addr"],
-        shdr["sh_offset"],
-        shdr["sh_size"],
-      ))
-
-  def get_data(self):
-    return self._data
--- elf_packer/packer.py
+++ elf_packer/packer.py
@@ -14,9 +14,34 @@ def main():
   with open(sys.argv[1], "rb") as f:
     data = f.read()
 
-  elf = ELF64(data)
-  elf.add_section(bytearray([0x11, 0x22]), "asdf", 0, 0, address=0x123456)
-  elf.print()
+  elf = ELF(data)
+
+  address = 0x80000000
+
+  content = bytearray([0xcc] * 512)
+  section = Section()
+  section.type = Section.SHT_PROGBITS
+  section.size = len(content)
+  section.content = content
+  section.addralign = 16
+  section.addr = address
+
+  # segment = Segment()
+  # segment.type = Segment.PT_NULL
+  # segment.flags = Segment.PF_R | Segment.PF_X
+  # segment.offset = 0
+  # segment.vaddr = 0x8000_0000
+  # segment.paddr = 0
+  # segment.filesz = 512
+  # segment.memsz = 512
+  # segment.align = 16
+
+  # elf.append_segment(segment)
+  elf.append_section(section)
+  # elf.set_entry_point(address)
+
+  with open("%s.packed" % sys.argv[1], "wb") as f:
+    f.write(elf.data())
 
 if __name__ == "__main__":
   main()
